eafda0fc67398876a5f45f809723ed22742f827a
wollefitz <woamann@posteo.de>
2016-01-25 10:05:12 +0100

    Jekyll 3, Bootstrap 3.3.6, Bourbon 4.2.6, Bitters 1.2.0, Font Awesome 4.5.0

--- _pages/contact/contact.php
+++ _pages/contact/contact.php
@@ -1,34 +1,39 @@
 <?php
+/**
+ * Simple contact Form.
+ *
+ * @author Wolfgang Amann
+ */
+
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
+if(empty($_POST['name']) ||
+   empty($_POST['email']) ||
+   empty($_POST['message']) ||
+   !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		echo "No arguments Provided!";
+		return false;
+}
    
 // Vars   
 $name = $_POST['name'];
-$email_address = $_POST['email'];
+$emailAddress = $_POST['email'];
 $message = $_POST['message'];
-$empfaenger = "wolfgang@einundleipzig.de";
-$betreff = "einundleipzig Kontaktformular: " . $name;
+$to = "kontakt@einundleipzig.de";
+$subject = "einundleipzig Kontaktformular: " . $name;
 
 // Headers
 $from = "From: einundleipzig Kontakt <kontakt@einundleipzig.de>\r\n";
-$from .= "Reply-To: " . $email_address . "\r\n";
+$from .= "Reply-To: " . $emailAddress . "\r\n";
 $from .= "Content-Type: text/plain; charset=UTF-8\r\n";
 $from .= "Content-Transfer-Encoding: 8bit";
 
 // The actual text
 $text = "Neue Nachricht auf einundleipzig:\r\n";
 $text .= "Name: " . $name . "\r\n";
-$text .= "Email: " . $email_address . "\r\n\r\n";
+$text .= "Email: " . $emailAddress . "\r\n\r\n";
 $text .= "Nachricht:\r\n" . $message;
 
 // Send the mail
-mail($empfaenger, $betreff, $text, $from);
+mail($to, $subject, $text, $from);
 return true;	
 ?>
--- _plugins/vimeo.rb
+++ _plugins/vimeo.rb
@@ -1,15 +1,17 @@
 # A plugin for embedding videos from Vimeo using a simple Liquid tag, ie: {% vimeo 12345678 %}.
 # Based of the Youtube plugin from http://www.portwaypoint.co.uk/jekyll-youtube-liquid-template-tag-gist/
 module Jekyll
-class Vimeo < Liquid::Tag
+  class Vimeo < Liquid::Tag
 
-def initialize(name, id, tokens)
-super
-@id = id
+    def initialize(name, id, tokens)
+      super
+      @id = id.strip
+    end
+
+    def render(context)
+      %(<div class="vimeo-embed"><iframe width="100%" height="100%" src="http://player.vimeo.com/video/#{@id}" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>)
+    end
+  end
 end
-def render(context)
-%(<div class="vimeo-embed"><iframe width="100%" height="100%" src="http://player.vimeo.com/video/#{@id}" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>)
-end
-end
-end
+
 Liquid::Template.register_tag('vimeo', Jekyll::Vimeo)
