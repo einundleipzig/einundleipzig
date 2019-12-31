167320104040934537e8a9995f65ca6643639a7b
Wolfgang Amann <wolfgang.amann@camunda.com>
2019-12-31 22:29:05 +0100

    remove disqus

--- _assets/js/einundleipzig.js
+++ _assets/js/einundleipzig.js
@@ -2,7 +2,7 @@
 // Smooth scroll
 // ------------------------------------------------------------------------
 $(function() {
-  $('a[href^="#"]:not([href="#"]):not(.disqussion-link):not(.main-disqussion-link)').click(function() {
+  $('a[href^="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
